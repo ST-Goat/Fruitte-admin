f89009c2dcb64f240a5dc0fec9d788da5374a0c3
Duc Tran <mrtranduc1994@outlook.com>
2021-11-27 13:25:07 +0000

    Feature/reservation

--- src/pages/FarmManagement/FarmDetail/Activities.tsx
+++ src/pages/FarmManagement/FarmDetail/Activities.tsx
@@ -5,6 +5,9 @@ import Text from "pages/common/components/Text";
 import ButtonCustomizer from "pages/common/Button";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import NoActivities from "../components/NoActivities";
+import { Link } from "react-router-dom";
+
+import { farmManagementActivityUrl } from "routes";
 
 function Activities() {
   const { i18n, t } = useTranslation();
@@ -13,15 +16,17 @@ function Activities() {
   return (
     <div>
       <div className="flex justify-end mt-8 mb-16">
-        <ButtonCustomizer
-          className="flex justify-center items-center w-64 rounded rounded-3xl"
-          color="secondary"
-        >
-          <AddCircleIcon />
-          <Text className="ml-2">
-            {t("pages.farmManagement.createActivity")}
-          </Text>
-        </ButtonCustomizer>
+        <Link to={`${farmManagementActivityUrl}/test-id`}>
+          <ButtonCustomizer
+            className="flex justify-center items-center w-64 rounded rounded-3xl"
+            color="secondary"
+          >
+            <AddCircleIcon />
+            <Text className="ml-2">
+              {t("pages.farmManagement.createActivity")}
+            </Text>
+          </ButtonCustomizer>
+        </Link>
       </div>
       {acitivities.length > 0 ? "list" : <NoActivities i18n={i18n} />}
     </div>
